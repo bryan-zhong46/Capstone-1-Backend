6e8df95214256e953f66387f784adace9c3b876e
BRYAN ZHONG <bryan.zhong46@bmcc.cuny.edu>
2025-07-14 14:02:10 -0400

    Added a test GET route for polls.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const testDbRouter = require("./test-db");
+const pollsRouter = require("./polls")
 
 router.use("/test-db", testDbRouter);
+router.use("/polls", pollsRouter) // mount router for polls
 
 module.exports = router;
---
+++ api/polls.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const { Poll, User } = require("../database");
+
+// GET all polls
+router.get("/", async (req, res) => {
+    try {
+        res.send("This is the polls route.")
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error from the polls route.");
+    }
+});
