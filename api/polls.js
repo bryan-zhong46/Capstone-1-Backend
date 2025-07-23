6d79d93dcc09ade86db878293bbc9e92662d7059
bryan-zhong46 <bryan.zhong46@stu.bmcc.cuny.edu>
2025-07-23 15:46:45 +0000

    Added database url

--- api/polls.js
+++ api/polls.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-// const { Poll, User } = require("../database");
+const { Poll } = require("../database");
 // replit code
-const { Poll, User } = require("../dummy-database");
+// const { Poll, Option } = require("../dummy-database");
 
 // GET all polls
 router.get("/", async (req, res) => {
     try {
-        const polls = await Poll.findAll();
+        const polls = Poll.findAll();
         res.status(200).json(polls);
     } catch (error) {
         console.error(error);
@@ -18,7 +18,23 @@ router.get("/", async (req, res) => {
 // POST a new poll
 router.post("/", async (req, res) => {
     try {
-        const poll = await Poll.create(req.body);
+        const { pollData, pollOptions } = req.body;
+        console.log("POLLDATA", pollData);
+        console.log("POLLOPTIONS", pollOptions);
+        const poll = Poll.create({
+            title: pollData.title,
+            creator_id: pollData.creator_id,
+            description: pollData.description,
+            auth_required: pollData.auth_required,
+            expiration: pollData.expiration,
+        });
+        // loop over polloptions and create each
+        for (let i = 0; i < pollOptions.length; i++) {
+            option = pollOptions[i];
+            const option = await Poll.createOption(poll, option);
+        }
+
+        // const poll = await Poll.create(req.body); // route for pollData w/o pollOptions
         res.status(201).send(poll);
     } catch (error) {
         res.status(500).send("Failed to create poll");
--- api/users.js
+++ api/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const { Poll, User } = require("../database");
+const { User } = require("../database");
 // replit code
-const { Poll, User } = require("../dummy-database");
+// const { Poll, User } = require("../dummy-database");
 
 // GET all users
 router.get("/", async (req, res) => {
